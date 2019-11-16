8e3e12681de2ad393195ff6df2b1c0e58cdba269
Chinwe <c-bianca@hotmail.com>
2019-11-16 10:47:12 -0500

    prizes

--- src/views/Prizes.vue
+++ src/views/Prizes.vue
@@ -60,44 +60,50 @@
       <b-row>
         <b-col md="1" />
         <b-col sm="10" md="3">
-          <p class="category"> Best use of Transposit </p>
+          <p class="category"> AstraZeneca </p>
           <p class="prizeItem"> $50 Amazon Gift Card for Each Team Member </p>
-          <p class="info"> Only 24 hours to create your project? No problem, Create an app and compose different APIs together with the world's first API composition platform, Transposit.
+          <p class="info"> AstraZeneca will be offering 2 challenges this year. The first being environmental sustainability; how can we use things like AR, AI, Gamification, Mobile, AR, Wearables to improve health, footprint, and recycling? Digital Health is the topic for our second challenge; how can we use technology to ensure medication adherence for example?
           </p>
         </b-col>
         <b-col sm="10" md="3">
           <p class="category"> Bose </p>
-          <p class="prizeItem"> TBD </p>
-          <p class="info"> TBD </p>
+          <p class="prizeItem">Noise Cancelling Headphone 700 for 5 </p>
+          <p class="info"> Hack something with BoseAR SDK or Bose SoundTouch API! </p>
         </b-col>
         <b-col sm="10" md="3">
+          <p class="category"> Best use of Twilio </p>
+          <p class="prizeItem"> littleBits R2D2 Droid Kit, 4 </p>
+          <p class="info"> Create a hack using Twilio's powerful communications API. Do things like send SMS messages, phone calls, email, and much more in your project. The team that has the best use of Twilio will win a littleBits R2D2 Droid Kit per team member. </p>
+        </b-col>
+        <!-- <b-col sm="10" md="3">
           <p class="category"> Facebook </p>
           <p class="prizeItem"> TBD </p>
           <p class="info"> TBD </p>
-        </b-col>
+        </b-col> -->
       </b-row>
       <b-row>
         <b-col md="1" />
         <b-col sm="10" md="3" style="border-top: 1px solid grey; border-bottom: 1px solid grey;">
           <p class="category"> Capital One </p>
           <p class="prizeItem"> $250 Amazon Gift Card/team member </p>
-          <p class="info"> TBD
+          <p class="info"> We will give $250 Amazon Gift Card to each team member for the team who comes up with the Best Financial Hack.
           </p>
         </b-col>
         <b-col sm="10" md="3">
           <p class="category"> Raytheon Challenge </p>
-          <p class="prizeItem"> TBD </p>
-          <p class="info"> TBD </p>
+          <p class="prizeItem"> Google Home for 5 </p>
+          <p class="info"> Overall Tracks </p>
         </b-col>
         <b-col sm="10" md="3">
           <p class="category"> Best use of Google Cloud </p>
-          <p class="prizeItem"> Google Home Mini for Each Team Member </p>
-          <p class="info"> Build your hackathon project with a suite of secure storage, powerful compute, and integrated data analytics products provided by Google Cloud. See full list of products here: g.co/cloud. </p>
+          <p class="prizeItem"> Grand Prize: Cloud Patagonia Backpacks, Cloud Pillows, Cloud Trophies, and Cloud Water Bottles for each team member.<br>
+          Second Place: Google Home Minis for each team member. </p>
+          <p class="info"> Use any Google Cloud product <a href="cloud.google.com/products"> (cloud.google.com/products) </a> to be eligible. </p>
         </b-col>
       </b-row>
       <b-row>
         <b-col md="1" />
-        <b-col sm="10" md="3" style="border-top: 1px solid grey;">
+        <!-- <b-col sm="10" md="3" style="border-top: 1px solid grey;">
           <p class="category"> Best Domain Name from Domain.com </p>
           <p class="prizeItem"> Domain.com Backpack </p>
           <p class="info"> Register a domain using Domain.com during the weekend. Each team may submit one entry per person on the team. Each winning team member will receive a Domain.com branded backpack. </p>
@@ -106,12 +112,8 @@
           <p class="category"> Best Automation Hack with UiPath </p>
           <p class="prizeItem"> UiPath/MLH Backpack and an Internship Interview </p>
           <p class="info"> Build an automation hack using UiPath! Each winning team member will receive a UiPath/MLH Branded Backpack and an opportunity to interview for a UiPath Internship in 2020 with a high recommendation! </p>
-        </b-col>
-        <b-col sm="10" md="3">
-          <p class="category"> Best use of Twilio </p>
-          <p class="prizeItem"> $50 Amazon Gift Card for Each Team Member </p>
-          <p class="info"> Create a hack using Twilio's powerful communications API. Do things like send SMS messages, phone calls, email, and much more in your project. The team that has the best use of Twilio will win a $50 Amazon gift card per team member. </p>
-        </b-col>
+        </b-col> -->
+        
       </b-row>
 
 
@@ -136,7 +138,7 @@
 }
 
 a {
-  color: #ffffff;
+  color: #f05352;
   font-weight: 700;
 }
 
